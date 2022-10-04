57c18e887c45f87f6fbf5a47ad27a177ef4d19d5
DucMinhHuy <ducminhhuy99tbvn2@gmail.com>
2022-10-04 09:28:17 +0700

    thuchanh

--- src/main/java/com/bai11/controller/CustomerServlet.java
+++ src/main/java/com/bai11/controller/CustomerServlet.java
@@ -1,6 +1,7 @@
 package com.bai11.controller;
 
 import com.bai11.model.Customer;
+import com.bai11.sevice.ICustomerService;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,7 +14,7 @@ import java.util.List;
 
 @WebServlet(name = "CustomerServlet",urlPatterns = "/customers")
 public class CustomerServlet extends HttpServlet {
-    private CustomerServlet customerServlet=new CustomerServlet();
+    private ICustomerService customerService= (ICustomerService) new CustomerServlet();
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
         String action=request.getParameter("action");
@@ -23,13 +24,58 @@ public class CustomerServlet extends HttpServlet {
 
         switch (action){
             case "create":
+                createCustomer(request,response);
                 break;
             case "edit":
+                updateCustomer(request,response);
                 break;
             case "delete":
                 break;
             default:
                 break;
+        }
+    }
+    private void updateCustomer(HttpServletRequest request,HttpServletResponse response){
+        int id=Integer.parseInt(request.getParameter("id"));
+        String name=request.getParameter("name");
+        String email=request.getParameter("email");
+        String address=request.getParameter("address");
+        Customer customer=this.customerService.findById(id);
+        RequestDispatcher dispatcher;
+        if(customer==null){
+            dispatcher=request.getRequestDispatcher("error-404.jsp");
+        }else {
+            customer.setName(name);
+            customer.setAddress(address);
+            customer.setEmail(email);
+            this.customerService.update(id,customer);
+            request.setAttribute("customer",customer);
+            request.setAttribute("message","Customer information was update");
+            dispatcher=request.getRequestDispatcher("customerEdit.jsp");
+        }
+        try{
+            dispatcher.forward(request,response);
+        }catch (ServletException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    private void createCustomer(HttpServletRequest request,HttpServletResponse response){
+        String name=request.getParameter("name");
+        String email=request.getParameter("email");
+        String address=request.getParameter("address");
+        int id= (int) (Math.random()*10000);
+        Customer customer=new Customer(id,name,email,address);
+        this.customerService.save(customer);
+        RequestDispatcher dispatcher=request.getRequestDispatcher("customerCreate.jsp");
+        request.setAttribute("message","new customer was created");
+        try{
+            dispatcher.forward(request,response);
+        }catch (ServletException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
         }
     }
 
@@ -40,8 +86,10 @@ public class CustomerServlet extends HttpServlet {
         }
         switch (action){
             case "create":
+                showCreateForm(request,response);
                 break;
             case "edit":
+                showEditFrom(request,response);
                 break;
             case "detele":
                 break;
@@ -52,11 +100,70 @@ public class CustomerServlet extends HttpServlet {
                 break;
         }
     }
+    private void showDelete(HttpServletRequest request,HttpServletResponse response){
+        int id=Integer.parseInt(request.getParameter("id"));
+        Customer customer=this.customerService.findById(id);
+        RequestDispatcher dispatcher;
+        if(customer==null){
+            dispatcher=request.getRequestDispatcher("error-404.jsp");
+        }else{
+            request.setAttribute("customer",customer);
+            dispatcher=request.getRequestDispatcher("customerDelete.jsp");
+        }try{
+            dispatcher.forward(request,response);
+        }catch (ServletException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    private void deleteCustomer(HttpServletRequest request,HttpServletResponse response){
+        int id=Integer.parseInt(request.getParameter("id"));
+        Customer customer=this.customerService.findById(id);
+        RequestDispatcher dispatcher;
+        if(customer==null){
+            dispatcher=request.getRequestDispatcher("error-404.jsp");
+        }else{
+            this.customerService.remove(id);
+            try{
+                response.sendRedirect("/customer");
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+    private void showEditFrom(HttpServletRequest request,HttpServletResponse response){
+        int id=Integer.parseInt(request.getParameter("id"));
+        Customer customer=this.customerService.findById(id);
+        RequestDispatcher dispatcher;
+        if(customer==null){
+            dispatcher=request.getRequestDispatcher("error-404.jsp");
+        }else{
+            request.setAttribute("customer",customer);
+            dispatcher=request.getRequestDispatcher("customerEdit.jsp");
+        }try{
+            dispatcher.forward(request,response);
+        }catch (ServletException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 
     private void listCustomers(HttpServletRequest request, HttpServletResponse response) {
-        List<Customer>customers=this.customerServlet.findAll();
+        List<Customer>customers=this.customerService.findAll();
         request.setAttribute("customers",customers);
         RequestDispatcher dispatcher=request.getRequestDispatcher("customerList.jsp");
+        try{
+            dispatcher.forward(request,response);
+        }catch (ServletException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    private void showCreateForm(HttpServletRequest request,HttpServletResponse response){
+        RequestDispatcher dispatcher=request.getRequestDispatcher("customerCreate.jsp");
         try{
             dispatcher.forward(request,response);
         }catch (ServletException e){
--- src/main/java/com/bai11/sevice/CustomerServicelmpl.java
+++ src/main/java/com/bai11/sevice/CustomerServicelmpl.java
@@ -8,38 +8,38 @@ import java.util.List;
 import java.util.Map;
 
 public class CustomerServicelmpl implements ICustomerService{
-    private static Map<Integer,Customer>customer=new HashMap<>();
+    private static Map<Integer,Customer>customers=new HashMap<>();
     static {
-        customer.put(1,new Customer(1,"huy","huy@.com","thai binh"));
-        customer.put(2,new Customer(2,"phong","phong@.com","quang ninh"));
-        customer.put(3,new Customer(3,"hung","hung@com","yenbai"));
-        customer.put(4,new Customer(4,"quyet","quyet@.com","bac ninh"));
-        customer.put(5,new Customer(5,"luong","luong@.com","bacminh"));
-        customer.put(6,new Customer(6,"linh","linh@.com","hanoi"));
+        customers.put(1,new Customer(1,"huy","huy@.com","thai binh"));
+        customers.put(2,new Customer(2,"phong","phong@.com","quang ninh"));
+        customers.put(3,new Customer(3,"hung","hung@com","yenbai"));
+        customers.put(4,new Customer(4,"quyet","quyet@.com","bac ninh"));
+        customers.put(5,new Customer(5,"luong","luong@.com","bacminh"));
+        customers.put(6,new Customer(6,"linh","linh@.com","hanoi"));
     }
 
     @Override
     public List<Customer> findAll() {
-        return new ArrayList<>(customer.values());
+        return new ArrayList<>(customers.values());
     }
 
     @Override
     public void save(Customer customer) {
-        customer.put(customer.getId(),customer);
+        customers.put(customer.getId(),customer);
     }
 
     @Override
     public Customer findById(int id) {
-        return customer.get(id);
+        return customers.get(id);
     }
 
     @Override
     public void update(int id, Customer customer) {
-customer.put(id,customer);
+customers.put(id,customer);
     }
 
     @Override
     public void remove(int id) {
-customer.remove(id);
+customers.remove(id);
     }
 }
